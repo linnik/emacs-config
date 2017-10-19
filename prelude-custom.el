a1b003e17285f8bd49f8f65de9239ba0a0e00cbd
Vyacheslav Linnik <vyacheslav.linnik@gmail.com>
2017-10-19 06:19:32 +0400

    enable smooth scrolling

--- prelude-custom.el
+++ prelude-custom.el
@@ -132,3 +132,11 @@
       (desktop-save desktop-dirname)))
 (add-hook 'auto-save-hook 'my-desktop-save)
 (add-hook 'focus-out-hook 'my-desktop-save)
+
+;; smooth scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 1)
+(setq mac-mouse-wheel-mode t)
+(setq mac-mouse-wheel-smooth-scroll t)
